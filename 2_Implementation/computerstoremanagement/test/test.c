1afdb30fcccfefc51824991ee28e6a2ad8c67cd3
stepin104553 <72563105+stepin104553@users.noreply.github.com>
2020-11-11 06:48:55 -0800

    Update test.c

--- 2_Implementation/computerstoremanagement/test/test.c
+++ 2_Implementation/computerstoremanagement/test/test.c
@@ -1,1 +1,21 @@
 
+#include "unity.h"
+#include "computerstoremanagement.h"
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_enterchoice(void)
+{
+  TEST_ASSERT(enterchoice(1));
+}
+
+int test_main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_enterchoice);
+
